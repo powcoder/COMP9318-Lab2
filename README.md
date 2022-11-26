fa98d0bed8b910f76507d8722899a8a13a2c193e
powcoder <powcoder@163.com>
2022-11-26 14:18:58 +0800

    COMP9318 Lab2

---
+++ README.md
@@ -0,0 +1,7 @@
+# COMP9318 Lab2
+# 加微信 powcoder
+
+# Programming Help Add Wechat powcoder
+
+# Email: powcoder@163.com
+
---
+++ submission.py
@@ -0,0 +1,18 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+## import modules here 
+import pandas as pd
+import numpy as np
+import helper
+
+
+################### Question 1 ###################
+
+def buc_rec_optimized(df):# do not change the heading of the function
+    pass # **replace** this line with your code    
